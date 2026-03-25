2a2dbf7e59fd03f3429efe02a8ea70321a9dd8f3
cnuzillat <cnuzillat@gmail.com>
2026-03-24 23:43:12 -0400

    Add project files.

---
+++ TaskItem.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskManagerAPI.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+
+        public int AssignedUserId { get; set; }
+        public User AssignedUser { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
